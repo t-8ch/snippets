cb2e193bfdf295dff41246977e72dfa8576064c8
Thomas Weißschuh <thomas@t-8ch.de>
2014-11-27 19:11:05 +0000

    [pandoc-mailcap] show proper help

--- PandocMailcap.hs
+++ PandocMailcap.hs
@@ -5,24 +5,33 @@ import Prelude hiding (readFile)
 import Data.ByteString.Lazy (readFile)
 import Data.Encoding (encodingFromString, decodeLazyByteString, DynEncoding)
 import Text.Pandoc (writePlain, readHtml, def)
-import System.Environment (getArgs)
+import System.Environment (getArgs, getProgName)
+import System.Exit (exitFailure, exitSuccess)
 
 defaultEncoding :: DynEncoding
 defaultEncoding = encodingFromString "UTF-8"
 
-parseArgs :: [String] -> (DynEncoding, FilePath)
-parseArgs [     ] = error "missing argument"
-parseArgs [f    ] = (defaultEncoding, f)
-parseArgs [f, ""] = (defaultEncoding, f)
-parseArgs [f,  e] = (encodingFromString e, f)
-parseArgs _      = error "too many arguments"
+parseArgs :: [String] -> Maybe (DynEncoding, FilePath)
+parseArgs [     ] = Nothing
+parseArgs [f    ] = Just (defaultEncoding, f)
+parseArgs [f, ""] = Just (defaultEncoding, f)
+parseArgs [f,  e] = Just (encodingFromString e, f)
+parseArgs _       = Nothing
 
 convert :: String -> String
 convert = writePlain def . readHtml def
 
+printHelp :: IO ()
+printHelp = do
+    p <- getProgName
+    putStrLn $ "Usage: " ++ p ++ " FILE [ENCODING]"
+
 main :: IO ()
 main = do
-        args <- getArgs
-        let (e, f) = parseArgs args
-        c <- readFile f
-        putStrLn . convert $ decodeLazyByteString e c
+    args <- getArgs
+    case parseArgs args of
+        Nothing -> printHelp >> exitFailure
+        Just (e, f) -> do
+                c <- readFile f
+                putStr $ convert $ decodeLazyByteString e c
+                exitSuccess
